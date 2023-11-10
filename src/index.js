15c3753093b321ddde0ccfe624d451d3ae8ecd98
dylewski <130579182+dylewskii@users.noreply.github.com>
2023-11-10 12:24:27 +0100

    refactor render function

--- src/index.js
+++ src/index.js
@@ -12,23 +12,35 @@ import { addTask } from './modules/addTask';
 import { deleteTask } from './modules/deleteTask';
 import { editTask } from './modules/editTask';
 // display
-import { render } from './modules/render';
+import { display } from './modules/render';
 
 // --- Main ---
 export const allProjects = {
-    home: {},
-    gym: {},
-    work: {},
+    home: {
+        "do shopping": ["do groceries", "Need to get groceries", 'hi']
+    },
+
+    gym: {
+        "workout": ["workout", "10 push ups", "hi"],
+        "swim": ["swim", "10 laps", "med"],
+    },
+
+    food: {
+        "cook food": ["cook food", "Need to make dumplings", 'low'],
+        "make bread": ["make bread", "Need to make bread", 'med']
+    },
 };
 
-let task1 = addTask("Cook Food", "Need to make dumplings", 'low');
-let task2 = addTask("Play Futbol", "Need to play futball", 'high', '10/10/2010', "home");
+// let task1 = addTask("Cook Food", "Need to make dumplings", 'low');
+// let task2 = addTask("Play Futbol", "Need to play futball", 'high', '10/10/2010', "home");
 // let task3 = addTask("Complete Coursework", "Maths needs completing", '14/14/2014', 'med', "work");
 
-allProjects[task1.project][task1.title] = task1;
-allProjects[task2.project][task2.title] = task2;
-console.log(allProjects)
+// allProjects[task1.project][task1.title] = task1;
+// allProjects[task2.project][task2.title] = task2;
+// console.log(allProjects)
+const displayController = display();
 
 
-document.addEventListener("DOMContentLoaded", render());
+document.addEventListener("DOMContentLoaded", displayController.renderTabs());
+document.addEventListener("DOMContentLoaded", displayController.handleTabClick());
 
--- src/modules/render.js
+++ src/modules/render.js
@@ -1,77 +1,122 @@
 import { allProjects } from "..";
 
-export function render() {
+export const display = function() {
+    const controller = {};
     const allProjectsArr = Object.keys(allProjects);
     const tabContainer = document.querySelector(".tab-container");
     const taskContainer = document.querySelector(".task-container");
+    const content = document.querySelector(".content");
 
-    allProjectsArr.forEach((project, index) => {
-        const newTab = document.createElement("button");
-        newTab.textContent = `${project.toUpperCase()}`;
-        newTab.classList.add("tab-btn");
-        newTab.setAttribute("data-for-tab", `${index + 1}`);
-        tabContainer.appendChild(newTab);
+    function capitalizedWords(str) {
+        const words = str.split(' ');
+        const capitalizedWords = words.map(word => {
+            if (word.length > 0) {
+                return word.charAt(0).toUpperCase() + word.slice(1);
+            } else {
+                return '';
+            }
+        });
+        
+        return capitalizedWords.join(' ');
+    }
 
-        newTab.addEventListener("click", (e) => {
-            const tabName = e.target.textContent.toLowerCase();
-            const tabIndex = e.target.dataset.forTab - 1;
-            const projectTasks = Object.keys(allProjects[tabName])
+    const renderTabs = function() {
+        while (tabContainer.firstChild) {
+            tabContainer.removeChild(tabContainer.firstChild);
+        }
 
-            projectTasks.forEach(task => {
-                // task div
-                const taskDiv = document.createElement("div");
-                taskDiv.classList.add("task");
-                taskDiv.setAttribute("data-tab", `${tabIndex}`)
-                taskContainer.appendChild(taskDiv)
-                
-                // first child container
-                const taskLabel = document.createElement("label");
-                taskLabel.classList.add("task__checkbox");
-                taskDiv.appendChild(taskLabel);
+        allProjectsArr.forEach((project, index) => {
+            const newTab = document.createElement("button");
+            newTab.textContent = `${capitalizedWords(project)}`;
+            newTab.classList.add("tab-btn");
+            newTab.setAttribute("data-for-tab", `${index + 1}`);
+            tabContainer.appendChild(newTab);
+        })
+    };
 
-                const taskInput = document.createElement("input");
-                taskInput.setAttribute("type", "checkbox");
-                taskInput.classList.add("nes-checkbox", "checkbox");
-                taskLabel.appendChild(taskInput);
+    const handleTabClick = function() {
+        const tabBtns = document.querySelectorAll(".tab-btn");
 
-                const taskSpan = document.createElement("span");
-                taskLabel.appendChild(taskSpan);
+        tabBtns.forEach(btn => {
+            btn.addEventListener("click", (e) => {
+                const tabName = e.target.textContent.toLowerCase();
+                const tabIndex = e.target.dataset.forTab - 1;
+                const projectTasks = Object.keys(allProjects[tabName])
 
-                // second child container
-                const taskTitle = document.createElement('div');
-                taskTitle.classList.add('task__title');
+                // Add active class only to current button.
+                tabBtns.forEach(otherBtn => {
+                    otherBtn.classList.remove("tab--active");
+                });
+                btn.classList.add("tab--active");
 
-                const p = document.createElement('p');
-                p.textContent = 'Walk';
+                // Clear task container
+                while (taskContainer.firstChild) {
+                    taskContainer.removeChild(taskContainer.firstChild);
+                }
 
-                taskTitle.appendChild(p);
-                taskDiv.appendChild(taskTitle);
-
-                // third child container
-                const taskControls = document.createElement('div');
-                taskControls.classList.add('task__controls');
-
-                const infoBtn = document.createElement('button');
-                infoBtn.classList.add('task-btn', 'nes-btn');
-                infoBtn.id = 'infoBtn';
-                infoBtn.textContent = 'Info';
-
-                const editBtn = document.createElement('button');
-                editBtn.classList.add('task-btn', 'nes-btn');
-                editBtn.id = 'editBtn';
-                editBtn.textContent = 'Edit';
-
-                const deleteBtn = document.createElement('button');
-                deleteBtn.classList.add('task-btn', 'nes-btn', 'is-error');
-                deleteBtn.id = 'deleteBtn';
-                deleteBtn.textContent = 'X';
-
-                taskControls.appendChild(infoBtn);
-                taskControls.appendChild(editBtn);
-                taskControls.appendChild(deleteBtn);
-
-                taskDiv.appendChild(taskControls);
+                console.log(projectTasks)
+                projectTasks.forEach(task => {
+                    // task div
+                    const taskDiv = document.createElement("div");
+                    taskDiv.classList.add("task");
+                    taskDiv.setAttribute("data-tab", `${tabIndex}`)
+                    taskContainer.appendChild(taskDiv)
+                    
+                    // first child container
+                    const taskLabel = document.createElement("label");
+                    taskLabel.classList.add("task__checkbox");
+                    taskDiv.appendChild(taskLabel);
+    
+                    const taskInput = document.createElement("input");
+                    taskInput.setAttribute("type", "checkbox");
+                    taskInput.classList.add("nes-checkbox", "checkbox");
+                    taskLabel.appendChild(taskInput);
+    
+                    const taskSpan = document.createElement("span");
+                    taskLabel.appendChild(taskSpan);
+    
+                    // second child container
+                    const taskTitle = document.createElement('div');
+                    taskTitle.classList.add('task__title');
+    
+                    const p = document.createElement('p');
+                    p.textContent = `${capitalizedWords(task)}`;
+    
+                    taskTitle.appendChild(p);
+                    taskDiv.appendChild(taskTitle);
+    
+                    // third child container
+                    const taskControls = document.createElement('div');
+                    taskControls.classList.add('task__controls');
+    
+                    const infoBtn = document.createElement('button');
+                    infoBtn.classList.add('task-btn', 'nes-btn');
+                    infoBtn.id = 'infoBtn';
+                    infoBtn.textContent = 'Info';
+    
+                    const editBtn = document.createElement('button');
+                    editBtn.classList.add('task-btn', 'nes-btn');
+                    editBtn.id = 'editBtn';
+                    editBtn.textContent = 'Edit';
+    
+                    const deleteBtn = document.createElement('button');
+                    deleteBtn.classList.add('task-btn', 'nes-btn', 'is-error');
+                    deleteBtn.id = 'deleteBtn';
+                    deleteBtn.textContent = 'X';
+    
+                    taskControls.appendChild(infoBtn);
+                    taskControls.appendChild(editBtn);
+                    taskControls.appendChild(deleteBtn);
+    
+                    taskDiv.appendChild(taskControls);
+                })
             })
         })
-    }) 
-}
+    }
+
+    controller.renderTabs = renderTabs;
+    controller.handleTabClick = handleTabClick;
+
+    return controller;
+  
+};
