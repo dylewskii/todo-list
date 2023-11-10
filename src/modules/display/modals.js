3bdb77fda405b32d0efb97924a22ab515a5e8961
dylewski <130579182+dylewskii@users.noreply.github.com>
2023-11-10 22:54:50 +0100

    build out addTask modal

---
+++ src/modules/display/modals.js
@@ -0,0 +1,10 @@
+import { allProjects } from "../..";
+
+const modal = function() {
+    const controller = {};
+
+    const addTask = function (currentTab){
+        const selectedTab = document.querySelector(".tab--active").textContent.toLowerCase();
+        
+    }
+}
--- src/modules/display/render.js
+++ src/modules/display/render.js
@@ -17,13 +17,11 @@ function changeCase(str, letterCase="uppercase") {
     return updatedWords.join(' ');
 };
 
-
 export const display = function() {
     const controller = {};
     const allProjectsArr = Object.keys(allProjects);
     const tabContainer = document.querySelector(".tab-container");
     const taskContainer = document.querySelector(".task-container");
-    const content = document.querySelector(".content");
 
     const renderTabs = function() {
         while (tabContainer.firstChild) {
@@ -52,7 +50,7 @@ export const display = function() {
             // task div
             const taskDiv = document.createElement("div");
             taskDiv.classList.add("task");
-            taskContainer.appendChild(taskDiv)
+            taskContainer.appendChild(taskDiv);
             
             // first child container
             const taskLabel = document.createElement("label");
@@ -68,33 +66,33 @@ export const display = function() {
             taskLabel.appendChild(taskSpan);
 
             // second child container
-            const taskTitle = document.createElement('div');
-            taskTitle.classList.add('task__title');
+            const taskTitle = document.createElement("div");
+            taskTitle.classList.add("task__title");
 
-            const p = document.createElement('p');
+            const p = document.createElement("p");
             p.textContent = `${changeCase(task)}`;
 
             taskTitle.appendChild(p);
             taskDiv.appendChild(taskTitle);
 
             // third child container
-            const taskControls = document.createElement('div');
-            taskControls.classList.add('task__controls');
+            const taskControls = document.createElement("div");
+            taskControls.classList.add("task__controls");
 
-            const infoBtn = document.createElement('button');
-            infoBtn.classList.add('task-btn', 'nes-btn');
-            infoBtn.id = 'infoBtn';
-            infoBtn.textContent = 'Info';
+            const infoBtn = document.createElement("button");
+            infoBtn.classList.add("task-btn", "nes-btn");
+            infoBtn.id = "infoBtn";
+            infoBtn.textContent = "Info";
 
-            const editBtn = document.createElement('button');
-            editBtn.classList.add('task-btn', 'nes-btn');
-            editBtn.id = 'editBtn';
-            editBtn.textContent = 'Edit';
+            const editBtn = document.createElement("button");
+            editBtn.classList.add("task-btn", "nes-btn");
+            editBtn.id = "editBtn";
+            editBtn.textContent = "Edit";
 
-            const deleteBtn = document.createElement('button');
-            deleteBtn.classList.add('task-btn', 'nes-btn', 'is-error');
-            deleteBtn.id = 'deleteBtn';
-            deleteBtn.textContent = 'X';
+            const deleteBtn = document.createElement("button");
+            deleteBtn.classList.add("task-btn", "nes-btn", "is-error");
+            deleteBtn.id = "deleteBtn";
+            deleteBtn.innerHTML = `X`;
             deleteBtn.addEventListener("click", () => {
                 deleteTask(task, selectedTab);
                 taskCounter();
@@ -126,8 +124,8 @@ export const display = function() {
                     otherBtn.classList.remove("tab--active");
                 });
                 btn.classList.add("tab--active");
-                taskCounter()
-                renderTasks()
+                taskCounter();
+                renderTasks();
             })
         })
 
